574ddf65afa2fc28bbf02d6c5274a8a3d6735499
Alex Hoenig <delimey@pm.me>
2023-11-21 15:43:06 +0000

    add API token request to Open Trivia database

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-import gspread
+import gspread # needed for access to google sheets to store user wrong questions
+import html #needed to decode html specials in quiz questions
+import requests
+
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
+# connect to google sheets
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -13,8 +17,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('training_questions')
 
-questions = SHEET.worksheet('questions')
+#connect to Open Trivia database
+url = "https://opentdb.com/api_token.php?command=request"
+response = requests.get(url)
+response_json = response.json()
+token = response_json['token'] #store session token - used once per game to prevent duplicate questions
 
-data = questions.get_all_values()
 
-pprint(data)
+
+
+
+
+# questions = SHEET.worksheet('questions')
+
+# data = questions.get_all_values()
+
+# pprint(data)
