730752f3588ca69f0af15a9454e1697d026fb62a
Alex Hoenig <delimey@pm.me>
2023-11-22 11:51:15 +0000

    refactor code to create dynamic function for question request

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import gspread # needed for access to google sheets to store user wrong question
 import html #needed to decode html specials in quiz questions
 import requests #needed to request questions from open trivia DB API
 import json #needed to help parse the recieved json strings
-import os #access to CLi clear command to clear out previous text
+import os #access to cli clear command to clear out previous text
 
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -19,35 +19,68 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('training_questions')
 
-#connect to Open Trivia database
-url = "https://opentdb.com/api_token.php?command=request"
-response = requests.get(url)
-response_json = response.json()
-token = response_json['token'] #store session token - used once per game to prevent duplicate questions
+def generate_new_token():
+    """
+    requests a new token from open trivia API and returns the string only
+    """
+    url = "https://opentdb.com/api_token.php?command=request"
+    response = requests.get(url)
+    response_json = response.json()
+    return response_json['token'] #used once per game to prevent duplicate questions
 
-int_number_of_questions = 10
-url_questions = f'amount={int_number_of_questions}'
 
-category = 20
-url_category = f'&category={category}'
+def get_questions(number, category, question_type, difficulty, token):
+    """
+    Main function to generate API query string from given inputs.
+    Token is required per session to prevent repeated questions
+    returns a json dictionary containing requested number of questions from given category
+    omitting parameters opens up request to ALL categories or difficulties
+    default number of questions is 10
+    number, category are integers, difficulty = 'easy', 'medium', 'hard' strings
+    """
+    
+    url_no_of_questions = f'amount={number}'
+    if category == 0:
+        url_category = ""
+    else:
+        url_category = f'&category={category}'
 
-difficulty = 'hard'
-url_difficulty = f'&difficulty={difficulty}'
+    if question_type == "":
+        url_type = ""
+    else:
+        url_type = f'&type={question_type}'
 
-url_token = f'&token={token}'
+    if difficulty == "":
+        url_difficulty = ""
+    else:
+        url_difficulty = f'&difficulty={difficulty}'
 
-questions_url = ('https://opentdb.com/api.php?' + url_questions + url_category + url_difficulty + url_token)
+    url_token = f'&token={token}'
 
-questions = requests.get(questions_url)
-questions_json = questions.json()
+    questions_url = ('https://opentdb.com/api.php?' + url_no_of_questions + url_category + url_difficulty + url_type + url_token)
+    print(questions_url)
+    questions = requests.get(questions_url)
+    questions_json = questions.json()
+    return questions_json
 
-print(questions_json)
+def get_categories():
+    category_list = requests.get('https://opentdb.com/api_category.php')
+    category_list_json = category_list.json()
+    return category_list_json
 
-category_list = requests.get('https://opentdb.com/api_category.php')
-category_list_json = category_list.json()
-print(f'Available Categories:\n {category_list_json}')
 
-os.system('clear')
+os.system('clear') #clear the cli first
+
+cat = 0
+question_type = ''
+diff = 'easy'
+num = 20
+tok = generate_new_token()
+print(get_questions(num, cat, question_type, diff, tok))
+
+
+
+
 
 # questions = SHEET.worksheet('questions')
 
